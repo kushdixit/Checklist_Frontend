b3b83b7df07ac56624a4068cccdd0fd6bcd4cc66
parul.j <parul.j@affinidi.com>
2022-09-16 16:27:10 +0530

    fix: style code fixes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,19 +1,19 @@
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 import Router from "components/Router";
-import Layout from 'components/Layout';
-import Spinner from 'components/Spinner'
-import './App.css';
-const App=({Component,pageProps})=> {
-  const loaderVisible = useSelector((state) => state.loader.loaderVisible)
+import Layout from "components/Layout";
+import Spinner from "components/Spinner";
+import "./App.css";
+const App = () => {
+  const loaderVisible = useSelector((state) => state.loader.loaderVisible);
   return (
     <div className="App">
       {loaderVisible && <Spinner />}
       <header className="App-header">
-          <Layout children={<Component {...pageProps} />} />
-          <Router/>
+        <Layout />
+        <Router />
       </header>
     </div>
   );
-}
+};
 
 export default App;
