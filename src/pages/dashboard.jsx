e4ca48c64bcead026e2df9633b905a92191297cf
parul.j <parul.j@affinidi.com>
2022-09-26 17:06:43 +0530

    fix: googlAuth

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Button from "components/Button";
 import { BodyContainer } from "styles/pages/Dashboard";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const {state} = useLocation()
+
+  console.log('state ==========', state.loginData)
 
   const addCheckList = () => {
     navigate("/check-list");
   };
 
+  
+
   return (
     <BodyContainer>
       Your Checklist
       <Button handleClick={addCheckList}>Plus</Button>
+      
     </BodyContainer>
   );
 };
