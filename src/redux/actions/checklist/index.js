7059f16ef3130adb72f4be1da989ca2a17d26520
sourabhsainii <sourabh.saini@thewitslab.com>
2022-10-21 15:44:56 +0530

    feat:task delete api

--- src/redux/actions/checklist/index.js
+++ src/redux/actions/checklist/index.js
@@ -1,4 +1,5 @@
 import axioPath from "api/axioPath";
+import axios from "axios";
 import { UPDATE_DATA } from "redux/actions/action_types";
 
 export const getChecklistBySubcategory = (subcategoryId) => (dispatch) =>
@@ -30,3 +31,71 @@ export const addNewTask = (data) => (dispatch) =>
       }
       return { error: true, data: ex };
     });
+
+export const deleteTask = () => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  console.log("asfd");
+  const payload = {
+    id: 64,
+  };
+  console.log(payload);
+  try {
+    const response = await fetch(
+      "http://112.196.2.202:8080/api/v1/Task/tasks",
+      {
+        method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(payload),
+      }
+    );
+
+    // const response = await axios.delete("http://112.196.2.202:8080/api/v1/Task/tasks", config, payload, {
+    //     hideLoader: false,
+    //   });
+    //   console.log("response", response);
+    //   dispatch({ type: UPDATE_DATA, payload: response.data
+    // });
+    // return { error: false, data: response.data };
+  } catch (ex) {
+    if (typeof ex == "string") {
+      console.log("err", ex);
+      return { ex: { message: ex } };
+    }
+    return { error: true, data: ex };
+  }
+};
+
+export const editTask = () => async (dispatch) => {
+  console.log("asfd");
+  const payload = {
+    id: 68,
+    taskName: "dsfdsf",
+  };
+  console.log(payload);
+  try {
+    const response = await axioPath.put("v1/Task/tasks", payload, {
+      hideLoader: false,
+    });
+    console.log("response", response);
+    dispatch({ type: UPDATE_DATA, payload: response.data });
+    // return { error: false, data: response.data };
+  } catch (ex) {
+    if (typeof ex == "string") {
+      console.log("err", ex);
+      return { ex: { message: ex } };
+    }
+    return { error: true, data: ex };
+  }
+};
