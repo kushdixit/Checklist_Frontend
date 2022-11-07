5fb4b1cb81c8bfd717149cf45b5da7dad99d37d2
Pooja Garg <erpooja06@gmail.com>
2022-11-07 17:32:03 +0530

    fix: create checklist pop-up

--- src/components/ChecklistCards.jsx
+++ src/components/ChecklistCards.jsx
@@ -1,40 +1,109 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import FirstImage from "assets/images/firstimage.jpg";
 import SecondImage from "assets/images/secondimage.jpg";
 import ThirdImage from "assets/images/thirdimage.jpg";
 import { useDispatch, useSelector } from "react-redux";
 import FourthImage from "assets/images/firstimage.jpg";
+import Button from "components/Button";
+import TextInput from "components/FormElements/TextInput";
 import {
   FirstSection,
   NewSection,
   SubSectionNew,
   CardMainSection,
+  Footer,
+  Morecontent,
+  IconInputField,
+  SelectFieldSection,
+  ButtonSection
 } from "styles/components/Card";
+import SelectField from 'components/FormElements/SelectField'
 import { getChecklist } from "redux/actions/checklist";
 import Card from "./Card";
-
+import { useForm, Controller } from "react-hook-form";
 const ChecklistCards = ({ item }) => {
   const dispatch = useDispatch();
   // const allChecklist = useSelector((state) => state.task?.allChecklist);
-
+  const [isCheck, setIsCheck] = useState(false);
+  
   const Checklist = [
     { id: 1, time: "3:25 p.m", image: FirstImage },
     { id: 2, time: "3:25 p.m", image: SecondImage },
     { id: 3, time: "3:25 p.m", image: ThirdImage },
     { id: 4, time: "3:25 p.m", image: FourthImage },
   ];
-
+  const {
+    handleSubmit,
+ 
+    control,
+  } = useForm({
+    mode: "onSubmit",
+    reValidateMode: "onBlur",
+    shouldFocusError: true,
+  });
   useEffect(() => {
     dispatch(getChecklist());
   }, []);
+  const dealAnalysisArr = [
+    {
+      label: 'checklist 1',
+      value: 'checklist 1',
+      id: '0',
+    },
+    {
+      label: 'checklist 2',
+      value: 'checklist 2',
+      id: '1',
+    },
+    {
+      label: 'checklist 3',
+      value: 'checklist 3',
+      id: '2',
+    },
+  ]
 
+
+
+  console.log(isCheck)
   return (
     <>
       <NewSection>
         <SubSectionNew>
           <h2> {item.templateName}</h2>
         </SubSectionNew>
-      </NewSection>
+        <Footer>
+          <Button onClick={() => setIsCheck(!isCheck)}>
+          Create Checklist
+          
+          </Button>
+       
+          </Footer>
+          {!isCheck ? (
+            <Morecontent>
+             <IconInputField>
+            <TextInput
+              name=""
+              type="text"
+              placeholder="Find Something Hereeee"
+              control={control}
+            />
+            
+          </IconInputField>
+          <SelectFieldSection>
+          <SelectField data={dealAnalysisArr} name="dealAnalysis1" control={control} placeholder="Select" />
+          </SelectFieldSection>
+          <ButtonSection>
+          <Button>
+         Save
+          
+          </Button>
+          </ButtonSection>
+            </Morecontent>
+          ) : null}
+          </NewSection>
+          
+      
+    
       <CardMainSection>
         <FirstSection>
           {item.checklists
@@ -46,6 +115,7 @@ const ChecklistCards = ({ item }) => {
             })}
         </FirstSection>
       </CardMainSection>
+
     </>
   );
 };
