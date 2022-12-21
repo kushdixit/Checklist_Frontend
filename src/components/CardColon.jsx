794d8681592e8c50fdc5f107c3a6570f6c4cb812
Pooja Garg <erpooja06@gmail.com>
2022-12-21 17:37:36 +0530

    fix:checklist description api

--- src/components/CardColon.jsx
+++ src/components/CardColon.jsx
@@ -58,14 +58,10 @@ const CardColon = ({ item, cardType }) => {
   const statusHandler = async (id, status) => {
     const res = await dispatch(ChecklistCompleted(id, status));
     refetchtemplate(res);
-    if (res.error === false) {
+    if (res.error == false) {
       dispatch(getChecklistBySubcategory(id));
       setIsOpenSort(false);
-      openNotification(
-        status
-          ? "Your Checklist has been Completed"
-          : "Your Checklist has been Reset"
-      );
+      openNotification(status ? "Completed" : "Reset");
     } else openNotification(res?.data);
   };
 
