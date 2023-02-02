0f17f59f5a6f975bc91fe7195af08210933bf600
Pooja Garg <erpooja06@gmail.com>
2023-02-02 17:08:41 +0530

    fix: footer and font/color modal

---
+++ src/styles/components/Footer.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+export const Section = styled.div`
+  text-align: center;
+  padding-bottom: 60px;
+  background: #f8f8f8;
+`;
+
+export const FirstSection = styled.div`
+  font-size: 13px;
+  color: #007ccb;
+  font-weight: 400;
+  padding-bottom: 2px;
+`;
+export const SecondSection = styled.div`
+  font-size: 13px;
+  color: #aaa;
+`;
+
+export const SubSection = styled.div`
+  border-top: 1px solid #ebebeb;
+  width: 100%;
+  max-width: 1250px;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 20px;
+`;
